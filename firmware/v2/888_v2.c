8b1c1d7ade6779dd3f5fe4c5eed3617961df01df
tomazas <labai.tamsu@gmail.com>
2015-04-23 02:44:46 +0300

    Stable circular buffer recv USART.

--- firmware/v2/888_v2.c
+++ firmware/v2/888_v2.c
@@ -4,35 +4,69 @@
 #define uchar unsigned char
 #define uint unsigned int
 
-uchar display[2][8][8]; // 8x8x8 = (Z,Y,X)
+volatile uchar display[2][8][8]; // 8x8x8 = (Z,Y,X)
 volatile uchar frame = 0;
 volatile uchar temp = 1;
 volatile uchar layer = 0;
 
 #define FOSC        12000000L 	// mcu clock - 12MHz
-#define BAUD_RATE 	115200
+#define BAUD_RATE 	9600
 #define BAUD        (256 - FOSC / 32 / BAUD_RATE)
+#define MAX_BUFFER 32
+
+volatile bit busy;
+volatile uchar io_buffer[MAX_BUFFER];
+volatile int io_read = 0;
+volatile int io_write = 0;
 
 ///////////////////////////////////////////////////////////
 
 void init_uart()
 {
-		// Baud rate: 115200; Parity: None; Data Bits: 8
-    SCON = 0x5a;                //set UART mode as 8-bit variable baudrate
+    SCON = 0x50;                //set UART mode as 8-bit variable baudrate
     TMOD = 0x20;                //timer1 as 8-bit auto reload mode
     AUXR = 0x40;                //timer1 work at 1T mode
     TH1 = TL1 = BAUD;           //115200 bps
-    TR1 = 1;										//timer1 start*/
+    TR1 = 1;										//timer1 start
+		ES = 1;                 //Enable UART interrupt
+    EA = 1;                 //Open master interrupt switch
+}
+
+void uart_isr() interrupt 4 using 1
+{
+    if (RI)
+    {
+        RI = 0;             //Clear receive interrupt flag
+			
+				if (!((io_write+1)%MAX_BUFFER == io_read)) {
+					io_buffer[io_write] = SBUF;
+					io_write = (io_write+1)%MAX_BUFFER;
+				}
+    }
+    if (TI)
+    {
+        TI = 0;             //Clear transmit interrupt flag
+        busy = 0;           //Clear transmit busy flag
+    }
+}
+
+void send_uart(uchar dat)
+{
+    while (busy);           //Wait for the completion of the previous data is sent
+    busy = 1;
+    SBUF = dat;             //Send data to UART buffer
 }
 
 ///////////////////////////////////////////////////////////
 
-void send_uart(uchar dat)
+/*void send_uart(uchar dat)
 {
     while (!TI);                //wait pre-data sent
     TI = 0;                     //clear TI flag
     SBUF = dat;                 //send current data
-}
+}*/
+
+///////////////////////////////////////////////////////////
 
 void send_str(char* s)
 {
@@ -43,11 +77,22 @@ void send_str(char* s)
 
 ///////////////////////////////////////////////////////////
 
-uchar recv_uart()
+/*uchar recv_uart()
 {
     while (!RI);                //wait receive complete
     RI = 0;                     //clear RI flag
     return SBUF;                //return receive data
+}*/
+
+int recv_uart() {
+	uint value;
+	if (io_write-io_read == 0) { 
+		return -1;
+	}
+	
+	value = io_buffer[io_read];
+	io_read = (io_read+1)%MAX_BUFFER;
+	return value;
 }
 
 ///////////////////////////////////////////////////////////
@@ -120,7 +165,8 @@ void swap()
 
 void main()
 {
-	uchar value, i, 
+	int value;
+	uchar i, 
 			started = 0, 
 			received = 0;
 	
@@ -138,11 +184,13 @@ void main()
 	while(1) 
 	{
 		value = recv_uart();
+		if (value == -1) continue;
 		
 		if (!started) 
 		{
 			if (value == 0xF2) { // start receiving batch
 				started = 1;
+				send_str("started");
 			} //else if (value == 0xF0) { // clear only 
 				//value = recv_uart();
 				//clear(temp,value);
@@ -160,16 +208,19 @@ void main()
 			{
 				display[temp][received/8][received%8] = value;
 				received++;
+				send_uart(value);
 			}
 			
 			if (received >= 64) // overflow?
 			{
+				send_str("re-paint");
+				/*
 				for (i=0; i<10; i++) {
 					delay(8000);
 					point(1,1,1,1);
 					delay(8000);
 					point(1,1,1,0);
-				}
+				}*/
 				
 				swap();
 				received = started = 0;	
